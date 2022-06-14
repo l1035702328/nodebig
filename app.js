bd051ec881397868b670ad764cea12af08835551
v_jjinnlli <1156989490@qq.com>
2022-06-14 16:49:23 +0800

    鉴权

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+// 创建 express 的服务器实例
+const app = express()
+
+const cors = require('cors')
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+
+// 响应数据的中间件
+app.use(function (req, res, next) {
+    // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
+    res.cc = function (err, status = 1) {
+        res.send({
+            // 状态
+            status,
+            // 状态描述，判断 err 是 错误对象 还是 字符串
+            message: err instanceof Error ? err.message : err,
+        })
+    }
+    next()
+})
+
+const joi = require('joi')
+
+// 错误中间件
+app.use(function (err, req, res, next) {
+    // 数据验证失败
+    if (err instanceof joi.ValidationError) return res.cc(err)
+    // 捕获身份认证失败的错误
+    if (err.name === 'UnauthorizedError') return res.cc('身份认证失败！')
+    // 未知错误
+    res.cc(err)
+})
+
+
+// 导入配置文件
+const config = require('./config')
+// 解析 token 的中间件
+const expressJWT = require('express-jwt')
+// 使用 .unless({ path: [/^\/api\//] }) 指定哪些接口不需要进行 Token 的身份认证
+app.use(expressJWT({ secret: config.jwtSecretKey }).unless({ path: [/^\/api\//] }))
+
+const userRouter = require('./router/user')
+//路由加前缀
+app.use('/api', userRouter)
+
+app.get("/hello",(req,res)=>{
+    console.log("有权限进入")
+    res.send("有权限进入")
+})
+// 调用 app.listen 方法，指定端口号并启动web服务器
+app.listen(3007, function () {
+    console.log('api server running at http://127.0.0.1:3007')
+})
